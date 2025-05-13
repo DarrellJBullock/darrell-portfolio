4943558e98a5dd4c26eed4027164615971c4164e
DarrellJBullock <darrelljfresh@gmail.com>
2025-05-13 12:24:15 -0400

    Update Social.jsx
    Updated links to socials

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import { FaGithub, FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedinIn />, path: "" },
-  { icon: <FaInstagram />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/DarrellJBullock" },
+  { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/darrellbullock" },
+  { icon: <FaInstagram />, path: "https://instagram.com/djb215" },
+  { icon: <FaTwitter />, path: "https://x.com/djb215" },
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
